3fc89c19f22cc04cf2283fdd7ff06bc7f499b531
Agustin-Silveira <agustinsilveira86@gmail.com>
2026-08-31 00:35:02 -0300

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,17 @@
+
+        tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        slate: { 850: '#141e30', 900: '#0f172a', 950: '#020617' },
+                        clinical: { blue: '#0ea5e9', red: '#ef4444', orange: '#f97316', yellow: '#eab308', green: '#10b981' }
+                    },
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                        mono: ['JetBrains Mono', 'monospace'],
+                    }
+                }
+            }
+        }
+    
